9bab799ab4f2ced859cbfa5e0b205c561b90fde4
Grzegorz Smajdor <grzegorz.smajdor@gmail.com>
2012-08-25 22:06:04 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskapp::Application.config.secret_token = '77e700195cff77f520125b7dd69dc8662f9cdd63f1ecf219ac1bef076559a5a9767ae5cff5c1dd0d476b192df91f6bd77ab59dc5bec822964567778d2c394b95'
